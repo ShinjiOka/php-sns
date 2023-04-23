d863408d39304bf6858aa754668c75f620691c00
ShinjiOka <oshinji871@gmail.com>
2023-04-23 18:56:11 +0900

    feature/sns-3 ログイン機能作成

--- user/user_registration_success.php
+++ user/user_registration_success.php
@@ -1,19 +1,5 @@
 <?php
-// データベースに接続
-$dsn = 'mysql:dbname=sns;host=172.24.188.149;charset=utf8mb4';
-$user = 'root';
-$password = 'Okazaki8888_';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-try {
-    $pdo = new PDO($dsn, $user, $password, $options);
-} catch (PDOException $e) {
-    die('データベースに接続できませんでした。' . $e->getMessage());
-}
-
+require_once('../dbconnect.php');
 // フォームから送信されたデータを取得
 $username = $_POST['username'];
 $email = $_POST['email'];
@@ -89,6 +75,6 @@ $stmt->execute();
     <h1>ユーザー登録が完了しました！</h1>
     <p>ユーザー登録が成功しました。</p>
     <p>ログインページからログインしてください。</p>
-    <a href="login.php">ログインページへ</a>
+    <a href="../login/login.php">ログインページへ</a>
 </body>
 </html>
